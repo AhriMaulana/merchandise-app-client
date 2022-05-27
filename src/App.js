72c557b4824dd146e1f88cd545c40c3633c607f6
ahri <ahrimaulana97@gmail.com>
2022-05-28 04:40:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import Login from './page/login';
+import Register from "./page/register";
+import Home from "./page/home";
+import Formedit from "./components/editpage";
+import { API, setAuthToken } from './config/api'
+import React, { useEffect, useContext } from "react";
+import { UserContext } from "../src/contex/userContext";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const navigate = useNavigate();
+    const [state, dispatch] = useContext(UserContext);
+
+    useEffect(() => {
+        if (localStorage.token) {
+            setAuthToken(localStorage.token);
+        } else {
+            navigate('/')
+        }
+    }, [state]);
+
+    const checkUser = async () => {
+        try {
+            const response = await API.get("/checkAuth");
+
+            if (response.status === 404) {
+                return dispatch({
+                    type: "failed",
+                });
+            }
+
+            let payload = response.data.data.user;
+            payload.token = localStorage.token;
+
+            // Send data to useContext
+            dispatch({
+                type: "success",
+                payload,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        checkUser();
+    }, []);
+    return (
+
+        <Routes>
+            <Route exact path="/" element={<Register />} />
+            <Route exact path="/Login" element={<Login />} />
+            <Route exact path="/Home" element={<Home />} />
+            <Route exact path="/edit/:id" element={<Formedit/>} />
+        </Routes>
+
+    );
 }
 
 export default App;
